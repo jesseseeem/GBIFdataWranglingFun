c67e041598b05e8e9c5c8417d0b9820a600b18e4
Jesse Czekanski-Moir <66492388+jesseseeem@users.noreply.github.com>
2021-05-11 18:35:01 -0400

    added taxonRichness and totalObservations

--- generalizableFakeDiversityData.R
+++ generalizableFakeDiversityData.R
@@ -7,38 +7,38 @@ library(viridis)
 taxonRichness = 10000
 totalObservations = 5000000
 
-maxLat = runif(50000, min = -70, max = 90)
-minLat = runif(50000, min = -30, max = 30)
-fakeLats = (maxLat + minLat)/2 + rcauchy(50000, location = 0, scale = 1)
+maxLat = runif(taxonRichness, min = -70, max = 90)
+minLat = runif(taxonRichness, min = -30, max = 30)
+fakeLats = (maxLat + minLat)/2 + rcauchy(taxonRichness, location = 0, scale = 1)
 
 
-longCauchy = sample(c(125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 0, -125, -60, 45), 1000000, replace = TRUE) + rcauchy(1000000, location = 0, scale = 5)
+longCauchy = sample(c(125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 125, 0, -125, -60, 45), 10000, replace = TRUE) + rcauchy(10000, location = 0, scale = 5)
 
 longCauchySubset = longCauchy[ which(longCauchy < 180 & longCauchy > -180)]
-fakeLongs = sample(longCauchySubset, 50000, replace = TRUE)
+fakeLongs = sample(longCauchySubset, taxonRichness, replace = TRUE)
 
 ### generate lat/long data that are more evenly distributed, without a strong latitudinal diversity gradient
 ### will still result in beta diversity because of intraspecific clustering generated lower down
-fakeFakeLat = runif(50000, min = -70, max = 70)
-fakeFakeLong = runif(50000, min = -170, max = 170)
+fakeFakeLat = runif(taxonRichness, min = -70, max = 70)
+fakeFakeLong = runif(taxonRichness, min = -170, max = 170)
 
-fakeGenera = sample(100000:200000, 50000, replace = FALSE)
+fakeGenera = sample(100000:900000, taxonRichness, replace = FALSE)
 
 ### use meteR library to generate a species-abundance distribution informed by Maximum Entropy Theory of Ecology
 ### see https://besjournals.onlinelibrary.wiley.com/doi/full/10.1111/2041-210X.12625
-fakemeteSAD = meteESF(N0 = 5000000, S0 = 50000, E0 = NULL, minE = 1)
+fakemeteSAD = meteESF(N0 = totalObservations, S0 = taxonRichness, E0 = NULL, minE = 1)
 fakeSAD = sad(fakemeteSAD)
-fakeSADrepValues = fakeSAD$q(seq(0, 1, length = 50001))
+fakeSADrepValues = fakeSAD$q(seq(0, 1, length = (taxonRichness + 1)))
 
 ### use that species abundance distribution to repeat genus names
-fakeGeneraOccurrences = rep(fakeGenera, c(fakeSADrepValues[1:50000]))
+fakeGeneraOccurrences = rep(fakeGenera, c(fakeSADrepValues[1:taxonRichness]))
 ### lat & long data are repeated, but random values are added so not all conspecific occurrences are right on top of each other
 ### cauchy distribution has some good outliers occasionally (including some that will be off the grid)
-fakeLatOccurrences = rep(fakeLats, c(fakeSADrepValues[1:50000])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
-fakeLongOccurrences = rep(fakeLongs, c(fakeSADrepValues[1:50000])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
+fakeLatOccurrences = rep(fakeLats, c(fakeSADrepValues[1:taxonRichness])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
+fakeLongOccurrences = rep(fakeLongs, c(fakeSADrepValues[1:taxonRichness])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
 
-fakeFakeLatOccurences = rep(fakeFakeLat, c(fakeSADrepValues[1:50000])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
-fakeFakeLongOccurrences = rep(fakeFakeLong, c(fakeSADrepValues[1:50000])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
+fakeFakeLatOccurences = rep(fakeFakeLat, c(fakeSADrepValues[1:taxonRichness])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
+fakeFakeLongOccurrences = rep(fakeFakeLong, c(fakeSADrepValues[1:taxonRichness])) + rcauchy(length(fakeGeneraOccurrences), location = 0, scale = 0.5)
 
 randomLat = runif(length(fakeGeneraOccurrences), min = -90, max = 90)
 randomLong = runif(length(fakeGeneraOccurrences), min = -180, max = 180)
@@ -57,6 +57,7 @@ fakeOccurrenceData = fakeOccurrenceData[which(
 	), ]
 
 ### write out data as a CSV
+### don't forget to change fileName so it reflects approximate total richness and observations
 write.table(fakeOccurrenceData, "fakeGenera4daniel.csv", sep = ",", quote = FALSE)
 
 # hexFake <- ggplot(fakeOccurrenceData, aes(x = fakeLongOccurrences, y = fakeLatOccurrences)) +
@@ -105,7 +106,7 @@ ggplot(fakeOccurrenceData, aes(x = randomLong, y = randomLat, z = fakeGeneraOccu
 
 
 ### make a subset of the database that's only the 20 most common genera
-mostCommonGenera = fakeGenera[49980:50000]
+mostCommonGenera = fakeGenera[(taxonRichness - 20):taxonRichness]
 
 ### initialize the new data.frame, then do the other 19 common genera with a loop
 onlyCommonGenera = fakeOccurrenceData[which(fakeOccurrenceData$fakeGeneraOccurrences == mostCommonGenera[1]), ]
