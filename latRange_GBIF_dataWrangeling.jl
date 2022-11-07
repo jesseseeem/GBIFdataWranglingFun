f2ac67b044278ff46df2971102bcc77217a26f98
Jesse Czekanski-Moir <66492388+jesseseeem@users.noreply.github.com>
2022-11-07 16:05:27 -0500

    updated file with a number of small changes

--- latRange_GBIF_dataWrangeling.jl
+++ latRange_GBIF_dataWrangeling.jl
@@ -1,51 +1,52 @@
 ### if necessary, can use bash to convert occurrence datafile into csv 
+### first remove all commas from the .txt file (there are often commas)
 ### cat australia_echinoderms_occurrence.txt | tr "\\t" "," > australia_echinoderms_occurrence.csv
 
 
 using DataFrames, CSV, Tables, Random
 ### change file names to match which taxa you're actually filtering... 
 
-GBIF_coleo_NA = CSV.read("coleoptera_northAmerica_occurrence.txt", DataFrame; header = true);
+GBIF_basidio_NA = CSV.read("basidios_world_occurrence.txt", DataFrame; delim = "\t", header = true);
 
-rnum = size(GBIF_coleo_NA)[1]
+rnum = size(GBIF_basidio_NA)[1]
 
 ### reduce number of columns to just the data we're interested in, 
 ### and add two columns of random numbers we can use to sort the data into different subsets
 
-subset_GBIF_coleo_NA = DataFrame(lat = GBIF_coleo_NA.decimalLatitude, 
-               long = GBIF_coleo_NA.decimalLongitude,
-               order = GBIF_coleo_NA.order, 
-               family = GBIF_coleo_NA.family, 
-               genus = GBIF_coleo_NA.genus, 
-               genusKey = GBIF_coleo_NA.genusKey,
-		species = GBIF_coleo_NA.specificEpithet,
-		acceptedTaxonKey = GBIF_coleo_NA.acceptedTaxonKey,
-               gbifID = GBIF_coleo_NA.gbifID, 
-               iucnCat = GBIF_coleo_NA.iucnRedListCategory, 
-               taxonRank = GBIF_coleo_NA.taxonRank, 
-               occurrenceStatus = GBIF_coleo_NA.occurrenceStatus, 
+subset_GBIF_basidio_NA = DataFrame(lat = GBIF_basidio.decimalLatitude, 
+               long = GBIF_basidio.decimalLongitude,
+               umbrellaTaxon = repeat(["basidio"], rnum),
+               order = GBIF_basidio_NA.order, 
+               family = GBIF_basidio_NA.family, 
+               genus = GBIF_basidio_NA.genus, 
+               genusKey = GBIF_basidio_NA.genusKey,
+		species = GBIF_basidio_NA.specificEpithet,
+		acceptedTaxonKey = GBIF_basidio_NA.acceptedTaxonKey,
+               gbifID = GBIF_basidio_NA.gbifID, 
+               iucnCat = GBIF_basidio_NA.iucnRedListCategory, 
+               taxonRank = GBIF_basidio_NA.taxonRank, 
+               occurrenceStatus = GBIF_basidio_NA.occurrenceStatus, 
                randomsorter = (randn(rnum) .+ rand(-90:90,rnum)),
                randomsorter2 = (randn(rnum) .+ rand(-180:180,rnum)),
 		randomsorter3 = (randn(rnum) .+ rand(1000:9999,rnum))); 
 
 ### get rid of missing values
-subsubset = dropmissing(subset_GBIF_coleo_NA, [:lat, :long, :genusKey, :genus, :acceptedTaxonKey, :taxonRank]);
+subsubset = dropmissing(subset_GBIF_basidio_NA, [:lat, :long, :genusKey, :genus, :acceptedTaxonKey, :taxonRank]);
 ### filter for only taxa identified to species
 subsubSUBset = subset(subsubset, :taxonRank => ByRow(taxonRank -> taxonRank == "SPECIES"));
 ### write it out! 
-CSV.write("subset_GBIF_coleo_NA.csv", DataFrame(subsubSUBset), bufsize = 4194304000);
+CSV.write("subset_GBIF_basidio.csv", DataFrame(subsubSUBset), bufsize = 4194304000);
 
 ### make dataframe with the lat min, lat max, and count for each species (accepted taxon key)
-GBIF_coleo_aus_summary = combine(groupby(subsubSUBset, :acceptedTaxonKey), :lat => maximum, :lat => minimum, nrow);
+GBIF_basidio_summary = combine(groupby(subsubSUBset, :acceptedTaxonKey), :lat => maximum, :lat => minimum, nrow);
 
 ### add other columns
-insertcols!(GBIF_coleo_aus_summary, 5,  :continent => repeat(["australia"], size(GBIF_coleo_aus_summary)[1]));
 
-insertcols!(GBIF_coleo_aus_summary, 6,  :taxon => repeat(["coleoptera"], size(GBIF_coleo_aus_summary)[1]));
+insertcols!(GBIF_basidio_summary, 5,  :taxon => repeat(["basidio"], size(GBIF_basidio_summary)[1]));
 
-insertcols!(GBIF_coleo_aus_summary, 7,  :lat_range => GBIF_coleo_aus_summary.lat_maximum - GBIF_coleo_aus_summary.lat_minimum);
+insertcols!(GBIF_basidio_summary, 6,  :lat_range => GBIF_basidio_summary.lat_maximum - GBIF_basidio_summary.lat_minimum);
 
-insertcols!(GBIF_coleo_aus_summary, 8,  :lat_mid => (GBIF_coleo_aus_summary.lat_maximum + GBIF_coleo_aus_summary.lat_minimum)/2);
+insertcols!(GBIF_basidio_summary, 7,  :lat_mid => (GBIF_basidio_summary.lat_maximum + GBIF_basidio_aus_summary.lat_minimum)/2);
 
 ### write out summary stats file
-CSV.write("GBIF_coleo_aus_summaryStats.csv", DataFrame(GBIF_coleo_aus_summary), bufsize = 4194304000);
+CSV.write("GBIF_basidio_summary.csv", DataFrame(GBIF_basidio_summary), bufsize = 4194304000);
